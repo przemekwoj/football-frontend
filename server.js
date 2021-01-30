009c68e0056818c8f94f0a7cb3bf579073f3911d
przemekwoj <przemoemo@poczta.onet.pl>
2021-01-30 18:07:43 +0100

    next commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dietician'));
+app.use(express.static('./dist/football'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dietician/'}),
+    res.sendFile('index.html', {root: 'dist/football/'}),
 );
 
 // Start the app by listening on the default Heroku port
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dietician'`, () => {
+  it(`should have as title 'football'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dietician');
+    expect(app.title).toEqual('football');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dietician app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('football app is running!');
   });
 });
